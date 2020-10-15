e1ce304d13bcc84b66caa243f7c4211fa7d09a7b
henjones1973 <henjones1973@byui.edu>
2020-10-14 23:29:49 -0700

    test

---
+++ js/poundsToKilograms.js
@@ -0,0 +1,15 @@
+function poundsToKilograms(){
+  //INPUT 
+  let pounds = parseFloat(document.getElementById
+  ('pounds').value);
+
+// PROCESSING
+// Conversion factor for pounds to kilograms is
+
+let kilograms = 2.205
+
+// OUTPUT
+document.getElementById('output').textContent =
+kilograms.toFixed(1) + " Kilograms";
+}
+
