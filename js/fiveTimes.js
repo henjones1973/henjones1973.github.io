34744908dfda513db9ec31831312431d3be5c32d
henjones1973 <henjones1973@byui.edu>
2020-11-17 22:28:55 -0800

    upload

---
+++ js/fiveTimes.js
@@ -0,0 +1,11 @@
+
+function getAnswer()
+{let output = "";
+for ( let i = 1; i <= 12; i++)
+{ output += "5 X " + i + " = " + i * 5 + "<br>";}
+
+
+
+
+//OUTPUT
+let outtput = document.getElementById("output").innerHTML = output}
